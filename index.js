0447a5d2d214f1c5ae7202bec4edcc27907f44c1
Cristian Contreras <cristian.fca01@gmail.com>
2024-04-25 19:35:27 -0400

    primer commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const Jimp = require('jimp');
+const { v4: uuidv4 } = require('uuid');
+
+app.use(express.static('public'));
+app.use('/processed', express.static('processed'));
+
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/cargar', upload.single('imageFile'), async (req, res) => {
+    try {
+        const path = req.file ? req.file.path : req.body.imageUrl;
+        const image = await Jimp.read(path);
+        const processedImage = await image
+            .greyscale()
+            .resize(350, Jimp.AUTO);
+
+        const outputPath = `processed/${uuidv4()}.jpeg`;
+        await processedImage.writeAsync(outputPath);
+        res.send(`<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Imagen Procesada</title>
+            <link rel="stylesheet" href="/style.css">
+        </head>
+        <body>
+        <img src="${outputPath}" class="fullscreen-image">
+        <br>
+        <button onclick="window.history.back();" class="back-button">Volver</button>
+        </body>
+        </html>`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al procesar la imagen');
+    }
+});
